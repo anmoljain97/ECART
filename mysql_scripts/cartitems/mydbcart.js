cfbf633ded2cb0337cc8bcefbcb05037fb2986b6
anmol jain <anmol97j@gmail.com>
2018-06-22 12:34:52 +0530

    cart added

--- mysql_scripts/cartitems/mydbcart.js
+++ mysql_scripts/cartitems/mydbcart.js
@@ -11,11 +11,16 @@ const connection =mysql.createConnection(
 )
 function insertincart(title,description,price,quantity){
     return new Promise(function(resolve,reject){
-        connection.query(`INSERT INTO cart (title, description, price,quantity) VALUES(
-            )`,[title,description,price,quantity],function(err,res){
+        
+        connection.query(`INSERT INTO cart (title, description, price,quantity) VALUES(?,?,?,?
+            )`,[title,description,+(price.split(" ")[1]),quantity],function(err,res){
                 if(err){
+                    console.log('------');
+                    
                     reject(err);
                 }else{
+                    console.log("---------");
+                    
                     resolve();
                 } 
             })
@@ -34,9 +39,43 @@ function getallitemsfromcart(){
         })
     })
 }
+function getitemswithtitlefromcart(title){
+    
+    return new Promise(function(resolve,reject){
+        connection.query(`SELECT * FROM cart where title='${title}'`,
+        function(err,rows,cols){
+            if(err){
+                console.log(err);
+                
+                reject(err);
+            }else{
+
+                console.log(rows);
+                resolve(rows); 
+            }
+           
+        })
+    })
+}
+function updatequantity(title,quantity){
+    return new Promise(function(resolve,reject){
+        connection.query(`UPDATE cart SET quantity=${quantity} WHERE title='${title}'`,
+    function(err,rows){
+        if(err){
+            reject(err);
+        }else{
+            resolve(rows); 
+        }
+       
+        })
+    })
+    
+}
 exports=module.exports={
     getallitemsfromcart,
-    insertincart
+    insertincart,
+    getitemswithtitlefromcart,
+    updatequantity
 }
 
 
